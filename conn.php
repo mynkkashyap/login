dd607cd3f8a7a7b57ec1a0fe3baa61859a9fb834
mayank kashyap <78128668+mynkkashyap@users.noreply.github.com>
2021-06-29 18:34:21 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost","maynkxyz_sample","5121974sK@","maynkxyz_sample");
+$conn = mysqli_connect("localhost","maynkxyz_sample","password","maynkxyz_sample");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
